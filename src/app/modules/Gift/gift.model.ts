f88e5dd2d78c3f8f035403687c83795ceab9732a
nayem-upo <nayemislam0181@gmail.com>
2024-02-16 12:05:39 +0600

    added live link

--- src/app/modules/Gift/gift.model.ts
+++ src/app/modules/Gift/gift.model.ts
@@ -15,7 +15,7 @@ const giftSchema = new Schema<IGift>({
     material: { type: String, required: true },
     coupon: {
         couponCode: { type: String },
-        discountPercentage: { type: Number, required: true },
+        discountPercentage: { type: Number },
     },
 }, { timestamps: true });
 
